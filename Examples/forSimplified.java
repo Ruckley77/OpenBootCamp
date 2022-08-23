b2f677d47d82cc9899b1e8cb070980df44bb80dd
Juan Marulanda <marulo77@hotmail.com>
2022-08-23 11:02:04 -0500

    Added Simplified for statement

---
+++ Examples/forSimplified.java
@@ -0,0 +1,11 @@
+package Examples;
+
+public class forSimplified {
+    public static void main(String[] args) throws Exception {
+        int numeros[] = {1,2,3,4,5}; 
+
+        for (int i : numeros){
+            System.out.println(i);
+        } 
+    }
+}
