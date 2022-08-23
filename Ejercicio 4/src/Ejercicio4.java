97a8b2de29e4f66311a81c19ed9954b61e36cfc8
Juan Marulanda <marulo77@hotmail.com>
2022-08-23 10:39:51 -0500

    -m Changed some comments

--- Ejercicio 4/src/Ejercicio4.java
+++ Ejercicio 4/src/Ejercicio4.java
@@ -1,13 +1,15 @@
 public class Ejercicio4 {
     public static void main(String[] args) throws Exception {
-        var estacion = "0"; // El valor de la variable no es una estacion
+        var estacion = "0"; 
 
 
-        int numeroIf = 0; // El numero es igual a 0
-        int numeroWhile = 0; // Cuenta desde el 0 hasta el 2
-        int numeroDo = 0; //Cuenta solo el numero 0
-        int numeroFor = 0; // cuenta desde el 0 hasta el 3
+        int numeroIf = 0; 
+        int numeroWhile = 0; 
+        int numeroDo = 0; 
+        int numeroFor = 0; 
 
+
+        // El numero es igual a 0
         if (numeroIf > 0) {
             System.out.println("El numero es positivo");
         } else if (numeroIf == 0){
@@ -17,21 +19,27 @@ public class Ejercicio4 {
             System.out.println("El numero es negativo");
         }
 
+        // Cuenta desde el 0 hasta el 2
+
         while (numeroWhile < 3) {
             System.out.println(numeroWhile);
             numeroWhile++;
-        }
+        } 
+
+         //Cuenta solo el numero 0
         do {
             System.out.println(numeroDo);
         } while (numeroDo > 1); {
             numeroDo++;
         }
 
+        // cuenta desde el 0 hasta el 3
         for (int i = 0; i <= 3; i++) {
             System.out.println(numeroFor);
             numeroFor++;
         }
 
+        // El valor de la variable no es una estacion
         switch(estacion) {
             case "Verano":
                 System.out.println("Es Verano");
