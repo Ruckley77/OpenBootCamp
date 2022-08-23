2b8f2b1838b67387a7b6941f36e0d2cc1ec4d20c
Ruckley77 <65756695+Ruckley77@users.noreply.github.com>
2022-08-22 19:16:24 -0500

    Add files via upload

---
+++ Ejercicio 3.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        suma(19,23,35);
+        Coche miCoche = new Coche();
+        miCoche.sumarPuerta();
+        System.out.println(miCoche.puertas);
+    } 
+    public static int suma(int x,int y,int z ) {
+        int resultado = x + y + z;
+        System.out.println(resultado);
+        return resultado;
+    }
+}
+
+class Coche {
+    public int puertas = 4;
+    public void sumarPuerta(){
+        this.puertas++;
+    }
+}
