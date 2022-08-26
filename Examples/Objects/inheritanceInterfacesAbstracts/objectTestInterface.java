b8add0f0fbe97e6cf7f35a5cd71d2cdaecd9277d
Juan Marulanda <marulo77@hotmail.com>
2022-08-26 10:43:52 -0500

    Added objectTestInterface

---
+++ Examples/Objects/inheritanceInterfacesAbstracts/objectTestInterface.java
@@ -0,0 +1,41 @@
+package Examples.Objects.inheritanceInterfacesAbstracts;
+
+public class objectTestInterface {
+    public static void main(String[] args) throws Exception {
+        House house = new House();
+
+        CheckProperty(house);
+    }
+
+    public static void CheckProperty(Property property){
+        property.Type("Townhouse");
+        property.Color("Red");
+        property.Occupied(false);
+    }
+}
+
+
+interface Property {
+    void Type(String type);
+    void Color(String color);
+    void Occupied(boolean occupied);
+}
+
+class House implements Property{
+
+    public void Type(String type){
+        System.out.println(type);
+    }
+
+    public void Color(String color){
+        System.out.println(color);
+    }
+
+    public void Occupied(boolean occupied){
+        if (occupied == true){
+            System.out.println("House is occupied");
+        } else {
+            System.out.println("House is vacant");
+        }   
+    }
+}
