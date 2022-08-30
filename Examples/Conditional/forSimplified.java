da15e65ead989eedd722c258969a2a4f76302810
Juan Marulanda <marulo77@hotmail.com>
2022-08-30 15:00:33 -0500

    Ejercicio5 added

--- Examples/Conditional/forSimplified.java
+++ Examples/Conditional/forSimplified.java
@@ -3,7 +3,7 @@ package Examples.Conditional;
 public class forSimplified {
     public static void main(String[] args) throws Exception {
         int numeros[] = {10,20,30,40,50};
-         
+        
         for (int i : numeros){
             System.out.println(i);
         } 
