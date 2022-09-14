318a8eace794386e5bfac7925fdc5cf6d135b80c
Juan Marulanda <marulo77@hotmail.com>
2022-09-14 12:02:08 -0500

    Edited Ejercicio08

--- Javascript/Ejercicio08/Ejercicio08.js
+++ Javascript/Ejercicio08/Ejercicio08.js
@@ -3,40 +3,13 @@ function func() {
 }
 console.log(func());
 
-function delay(){
-    return myPromess
+async function delay(){
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
 }
-
-// const myPromess = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve(console.log("Hola soy una promesa"))
-//     }, 5000)
-// })
-
-// delay();
-
 
 function* generatePair(){
     let n = 0;
     while(true){
-        n++;
-        n = 2*n;
-        if (n === 10){
-            return n 
-        }
-        yield n
+        yield n += 2;
     }
 }
-
-const genPair = generatePair();
-
-console.log(genPair.next());
-console.log(genPair.next());
-console.log(genPair.next());
-console.log(genPair.next());
-console.log(genPair.next());
-console.log(genPair.next());
-console.log(genPair.next());
-console.log(genPair.next());
-console.log(genPair.next());
-console.log(genPair.next());
